b53deabf1690c06860b360219c54674b93d242eb
DiegoD <a20171086@pucp.edu.pe>
2024-04-17 23:39:06 -0500

    Update PrivateRoutes.jsx

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,10 +1,16 @@
 import { Outlet, Navigate } from 'react-router-dom'
+import { lazy, Suspense } from 'react';
+import DashboardLayout from 'src/layouts/dashboard';
 import { useAuth } from './AuthContext'
 
 const PrivateRoutes = () => {
   const {user} = useAuth()
 
-  return user ? <Outlet/> : <Navigate to="/login"/>
+  return user ? <DashboardLayout>
+    <Suspense>
+      <Outlet />
+    </Suspense>
+  </DashboardLayout> : <Navigate to="/login"/>
 }
 
 export default PrivateRoutes
