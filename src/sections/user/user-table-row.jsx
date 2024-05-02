cd47592321f4e6ad24672b5dad40b265c3c71769
Sebastian Valdez <a20191970@pucp.edu.pe>
2024-05-02 21:03:53 +0000

    deleted public

--- src/sections/user/user-table-row.jsx
+++ src/sections/user/user-table-row.jsx
@@ -185,7 +185,7 @@ export default function UserTableRow({
           <Checkbox disableRipple checked={selected} onChange={handleClick} 
           style={{ backgroundColor: "F9FAFB", color: 'black'}}/>
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="public/assets/images/avatars/avatar_1.jpg" alt="Avatar"
+            <img src="/assets/images/avatars/avatar_1.jpg" alt="Avatar"
                  style={{ width: 100, height: 100, borderRadius: '50%' }} />
             <div style={{ marginLeft: 16 }}> {/* Espacio entre la imagen y el texto */}
               <Typography variant="h6" component="div">
