ef2d787f09d8317321d8a78a8ccb909747c617cf
DiegoD <a20171086@pucp.edu.pe>
2024-05-01 19:41:58 -0500

    Cambio de colores

--- src/sections/error/not-found-view.jsx
+++ src/sections/error/not-found-view.jsx
@@ -45,12 +45,11 @@ export default function NotFoundView() {
           }}
         >
           <Typography variant="h3" sx={{ mb: 3 }}>
-            Sorry, page not found!
+            ¡Lo sentimos!, la pagina no fue encontrada
           </Typography>
 
           <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be
-            sure to check your spelling.
+            Perdón, no pudimos encontrar la página solicitada.
           </Typography>
 
           <Box
@@ -64,7 +63,7 @@ export default function NotFoundView() {
           />
 
           <Button href="/" size="large" variant="contained" component={RouterLink}>
-            Go to Home
+            Ir al dashboard
           </Button>
         </Box>
       </Container>
