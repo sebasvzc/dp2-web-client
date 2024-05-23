afe3d7663224c2896df35a36b6c16ad5911c17cd
Sebastian Valdez <a20191970@pucp.edu.pe>
2024-05-23 23:42:18 +0000

    local to serv

--- src/sections/cupones/view/cupon-view-detail.jsx
+++ src/sections/cupones/view/cupon-view-detail.jsx
@@ -113,7 +113,7 @@ export default function CuponDetail() {
         console.log(idParam)
         // Simulación de carga
         let response="";
-        response = await fetch(`http://localhost:3000/api/cupones/detalleCuponCompleto`, {
+        response = await fetch(`http://3.220.179.149/api/api/cupones/detalleCuponCompleto`, {
           method: 'POST',
           body: JSON.stringify({ id:idParam }),
           headers: {
@@ -169,7 +169,7 @@ export default function CuponDetail() {
         // Simulación de carga
 
         if(searchName===""){
-          response = await fetch(`http://localhost:3000/api/cupones/listarclientesxcupon?query=all&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
+          response = await fetch(`http://3.220.179.149/api/api/cupones/listarclientesxcupon?query=all&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
             method: 'GET',
 
             headers: {
@@ -180,7 +180,7 @@ export default function CuponDetail() {
 
           });
         }else{
-          response = await fetch(`http://localhost:3000/api/cupones/listarclientesxcupon?query=${searchName}&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
+          response = await fetch(`http://3.220.179.149/api/api/cupones/listarclientesxcupon?query=${searchName}&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
             method: 'GET',
 
             headers: {
@@ -216,7 +216,7 @@ export default function CuponDetail() {
 
         setDataClients(data2.clientesxCupon);
 
-        response = await fetch(`http://localhost:3000/api/cupones/listarcuponesxdiacanjeado?idParam=${idParam}`, {
+        response = await fetch(`http://3.220.179.149/api/api/cupones/listarcuponesxdiacanjeado?idParam=${idParam}`, {
           method: 'GET',
 
           headers: {
@@ -299,7 +299,7 @@ export default function CuponDetail() {
       }
 
       let response="";
-      response = await fetch(`http://localhost:3000/api/cupones/modificar`, {
+      response = await fetch(`http://3.220.179.149/api/api/cupones/modificar`, {
         method: 'POST',
         body: formData,
         headers: {
