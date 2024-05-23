37b2f82b459effa600eb399a94b6b90e8153b3eb
Sebastian Valdez <a20191970@pucp.edu.pe>
2024-05-23 23:42:18 +0000

    local to serv

--- src/sections/cupones/view/cupon-view-detail.jsx
+++ src/sections/cupones/view/cupon-view-detail.jsx
@@ -104,7 +104,7 @@ export default function CuponDetail() {
         console.log(idParam)
         // Simulación de carga
         let response="";
-        response = await fetch(`http://localhost:3000/api/cupones/detalleCuponCompleto`, {
+        response = await fetch(`http://3.220.179.149/api/api/cupones/detalleCuponCompleto`, {
           method: 'POST',
           body: JSON.stringify({
             id:idParam,
@@ -173,7 +173,7 @@ export default function CuponDetail() {
         // Simulación de carga
 
         if(searchName===""){
-          response = await fetch(`http://localhost:3000/api/cupones/listarclientesxcupon?permission=Gestion%de%Cupones&query=all&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
+          response = await fetch(`http://3.220.179.149/api/api/cupones/listarclientesxcupon?permission=Gestion%de%Cupones&query=all&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
             method: 'GET',
 
             headers: {
@@ -184,7 +184,7 @@ export default function CuponDetail() {
 
           });
         }else{
-          response = await fetch(`http://localhost:3000/api/cupones/listarclientesxcupon?permission=Gestion%de%Cupones&query=${searchName}&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
+          response = await fetch(`http://3.220.179.149/api/api/cupones/listarclientesxcupon?permission=Gestion%de%Cupones&query=${searchName}&idParam=${idParam}&page=${page}&pageSize=${pageSize}`, {
             method: 'GET',
 
             headers: {
@@ -220,7 +220,7 @@ export default function CuponDetail() {
 
         setDataClients(data2.clientesxCupon);
 
-        response = await fetch(`http://localhost:3000/api/cupones/listarcuponesxdiacanjeado?permission=Gestion%de%Cupones&idParam=${idParam}`, {
+        response = await fetch(`http://3.220.179.149/api/api/cupones/listarcuponesxdiacanjeado?permission=Gestion%de%Cupones&idParam=${idParam}`, {
           method: 'GET',
 
           headers: {
@@ -303,7 +303,7 @@ export default function CuponDetail() {
       }
 
       let response="";
-      response = await fetch(`http://localhost:3000/api/cupones/modificar`, {
+      response = await fetch(`http://3.220.179.149/api/api/cupones/modificar`, {
         method: 'POST',
         body: formData,
         headers: {
