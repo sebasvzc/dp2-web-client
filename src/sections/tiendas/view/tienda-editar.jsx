151a3b44d5ae24b9d416027bd35d9078037f6330
Lian Tume <lian.tume@pucp.edu.pe>
2024-05-30 18:28:41 -0500

    Update tienda-visualizar.jsx

--- src/sections/tiendas/view/tienda-editar.jsx
+++ src/sections/tiendas/view/tienda-editar.jsx
@@ -27,14 +27,12 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import Iconify from '../../../components/iconify';
-
 import IconButton from '@mui/material/IconButton';
 import TablePagination from '@mui/material/TablePagination';
+import Iconify from '../../../components/iconify';
 import { getCategoriaTiendas } from '../../../funciones/api';
 
 import UserTableToolbar from '../../user/user-table-toolbar';
-import { getTiendas, getTipoCupones } from '../../../funciones/api';
 import DashboardCuponClient from '../../overview/dashboardCuponClient';
 
 
