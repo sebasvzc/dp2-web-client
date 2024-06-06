660349243933c37bea3e49eb2ad9ffa776017a70
Lian Tume <lian.tume@pucp.edu.pe>
2024-06-05 20:13:49 -0500

    tamanhoDatos

--- src/sections/cupones/view/cupon-view-detail.jsx
+++ src/sections/cupones/view/cupon-view-detail.jsx
@@ -458,7 +458,7 @@ export default function CuponDetail() {
       <hr style={{ borderColor: 'black', borderWidth: '1px 0 0 0', margin: 0 }} />
       <Grid container spacing={5}  >
         <Grid item xs={3}>
-          <Box sx={{ borderRight: 1, borderColor: 'divider', height: '900px', paddingTop: 2 }}>
+          <Box sx={{ borderRight: 1, borderColor: 'divider', height: '650px', paddingTop: 2 }}>
 
             <List component="nav" aria-label="opciones de navegación">
               <ListItemButton
